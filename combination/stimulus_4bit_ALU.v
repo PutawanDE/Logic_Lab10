b56911de5bb367c37fb09f54a1f2a39ff470748a
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-10-17 22:28:16 +0700

    Add test fourth bit - 4bit ALU, wrap up doc

--- combination/stimulus_4bit_ALU.v
+++ combination/stimulus_4bit_ALU.v
@@ -21,48 +21,52 @@ module stimulus_4bit_ALU;
     end
 
     initial begin        
-        repeat(4) begin
+        repeat(8) begin
             b = 1'b0;
-            repeat(4) begin
+            repeat(8) begin
                 #5 b = b + 1'b1;    
             end
             #5 a = a + 1'b1;
         end
+        #5 a = a + 1'b1;
         a = 1'b0;
 
         c0 = 1'b1;
-        repeat(4) begin
+        repeat(8) begin
             b = 1'b0;
-            repeat(4) begin
+            repeat(8) begin
                 #5 b = b + 1'b1;    
             end
             #5 a = a + 1'b1;
         end
+        #5 a = a + 1'b1;
         a = 1'b0;
 
         c1 = 1'b1;
         c0 = 1'b0;
-        repeat(4) begin
+        repeat(8) begin
             b = 1'b0;
-            repeat(4) begin
+            repeat(8) begin
                 #5 b = b + 1'b1;    
             end
             #5 a = a + 1'b1;
         end
+        #5 a = a + 1'b1;
         a = 1'b0;
 
         c0 = 1'b1;
-        repeat(4) begin
+        repeat(8) begin
             b = 1'b0;
-            repeat(4) begin
+            repeat(8) begin
                 #5 b = b + 1'b1;    
             end
             #5 a = a + 1'b1;
         end
+        #5 a = a + 1'b1;
         
-        #400 $finish;
+        #1200 $finish;
     end
 
     initial
-        $monitor($time, " Output c1=%d, c0=%d, a=%d, b=%d, y=%b=%d, z=%d", c1, c0, a, b, y, y, z);
+        $monitor($time, " Output c1=%d, c0=%d, a=%b=%d, b=%b=%d, y=%b=%d, z=%d", c1, c0, a, a, b, b, y, y, z);
 endmodule
